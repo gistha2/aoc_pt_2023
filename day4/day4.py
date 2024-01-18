f98501539452cc159b7cd000591de5f1e43695c0
gistha2 <gistha2@gmail.com>
2024-01-18 15:24:41 +0000

    day 4 pt 2 try

--- day4/day4.py
+++ day4/day4.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
 #Process input
-input_file = 'input.txt'
+input_file = 'test.txt'
 input = open(input_file, "r")
-
+maxCardNum = 0
 def pt1(input):
+    global maxCardNum
     total_pts = 0
     for line in input:
+        maxCardNum += 1
         _,card_nums = line.split(':')
         win_nums,your_nums = card_nums.split('|')
         win_nums = win_nums.split()
@@ -14,7 +17,33 @@ def pt1(input):
         total_pts += pts
     print(total_pts)
 
+def dfs(root, adj_list):
+    print("node", root)
+    count = 1
+    neighbours = adj_list[root]
+    for neighbour in neighbours:
+        count += dfs(neighbour,adj_list)
+
+    return count
+
+
 def pt2(input):
+    adj_list = defaultdict(list)
+    card_idx = 1
+    input = open(input_file, "r")
+    for line in input:
+        _,card_nums = line.split(':')
+        win_nums,your_nums = card_nums.split('|')
+        win_nums = win_nums.split()
+        your_nums = your_nums.split()
+        winning_nums = [num for num in your_nums if num in win_nums]
+        cards_won = len(winning_nums)
+        adj_list[card_idx] = [*range(card_idx+1,min(maxCardNum,card_idx+cards_won+1))]
+        card_idx += 1
     
+    print(adj_list)
+    print(dfs(1,adj_list))
+
+
 pt1(input)
 pt2(input)
