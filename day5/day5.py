280d9f0f889d22270b5d8e3836306309753d0412
gistha2 <gistha2@gmail.com>
2024-02-22 16:09:04 +0000

    pt1

--- day5/day5.py
+++ day5/day5.py
@@ -1,7 +1,11 @@
 
 def create_map_function(ranges): 
      def _(x):
-        return x * ranges
+        x = int(x)
+        for r in ranges:
+            if x>=r[1] and x<=r[1]+r[2]:
+                return r[0]+(x-r[1])
+        return x 
      return _
 
 def process_almanac(input_file):
@@ -20,10 +24,16 @@ def process_almanac(input_file):
             temp_ranges = []
         else: 
             temp_ranges.append(line.split())
+            temp_ranges[-1] = [ int(x) for x in temp_ranges[-1] ]
 
-
-            
-    return map_list[0](1)
+    location_nums = []
+    for seed_num in seed_nos:
+        new_num = seed_num
+        for map in map_list:
+            new_num = map(new_num)
+        location_nums.append(new_num)
+    print(location_nums)
+    return min(location_nums)
 
 input_file = 'test.txt'
 print(process_almanac(input_file))
